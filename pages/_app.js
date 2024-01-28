9cb309da746928e4d09263e2c2400c97040bd19e
Tony Li <tony.lilui416@gmail.com>
2024-01-28 04:10:10 -0500

    finished navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import '../app/globals.css'; // Import your global CSS file
-
+import Head from "next/head";
+import "../app/globals.css"; // Import your global CSS file
+import Navbar from "./navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
         />
       </Head>
+      <Navbar></Navbar>
       <Component {...pageProps} />
     </>
   );
